d37d63ab1849852af0e94d824f23b7c665fec5c7
JMRP25 <131288383+JMRP25@users.noreply.github.com>
2025-08-21 18:44:07 -0600

    Add files via upload

---
+++ Proyecto de Progra 2025/ProyectoProgra2025/src/main/java/com/mycompany/proyectoprogra2025/ProyectoProgra2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoprogra2025;
+
+/**
+ *
+ * @author jose2
+ */
+public class ProyectoProgra2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
